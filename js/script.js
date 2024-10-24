5a85b1399a4355b7ac8ba4d81317ec04b53325f3
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-10-24 16:26:42 +0200

    ADD timer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 console.log('Simon Says');
+
+
+let count = 0;
+const countdown = document.getElementById('countdown');
+
+const timer = setInterval(() => {
+    count++;
+    countdown.textContent = count;
+
+    if (count >= 30) {
+        clearInterval(timer);
+        alert("Tempo scaduto!");
+    }
+}, 1000);
+
