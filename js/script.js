26f02936f9f9676e7554d5a4ce3a5fe5ed26ce39
Leonardo-Rosales <leonardorosaleshuerta@gmail.com>
2024-10-24 16:39:03 +0200

    ADD random numbers

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 console.log('Simon Says');
 
+//TIMER
 
 let count = 0;
 const countdown = document.getElementById('countdown');
 
 const timer = setInterval(() => {
     count++;
-    countdown.textContent = count;
+    countdown.innerText = count;
 
     if (count >= 30) {
         clearInterval(timer);
         alert("Tempo scaduto!");
     }
 }, 1000);
+
+//NUMERI RANDOM
+
+const randomNumbers = [];
+for (let i = 0; i < 5; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 50) + 1);
+}
+
+const randomNumbersElement = document.getElementById('numbers-list');
+randomNumbersElement.innerText = randomNumbers.join(' - ');
 
